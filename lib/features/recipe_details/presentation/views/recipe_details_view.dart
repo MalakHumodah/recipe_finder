50a426576215b8461c7d6345589decdabe1c9884
MalakHumodah <100753927+MalakHumodah@users.noreply.github.com>
2024-05-20 22:03:55 +0300

    - updated getRecipeDetails method for recipe_details_view.dart

--- lib/features/recipe_details/presentation/views/recipe_details_view.dart
+++ lib/features/recipe_details/presentation/views/recipe_details_view.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recipe_finder/features/home/presentation/bloc/home_bloc.dart';
